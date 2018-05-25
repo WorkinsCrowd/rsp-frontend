b90b14762998b2ecc04177fe827b7b2a7155f5df
Victor Kozlov <punkrus18@gmail.com>
2018-05-25 18:11:45 +0300

    add action hands style;

--- src/components/ActionHands/index.jsx
+++ src/components/ActionHands/index.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import injectSheet from "react-jss";
 import PropTypes from "prop-types";
 
+import rockImg from "../../assets/rock.jpg";
+import paperImg from "../../assets/paper.jpg";
+import scissorsImg from "../../assets/scissors.jpg";
+
 const styles = {
   actionBox: {
     display: "flex",
-    "justify-content": "center"
+    "justify-content": "center",
+    alignItems: "center"
   },
   button: {
     width: "110px",
     height: "35px"
+  },
+  playerHand: {
+    transform: "scaleX(-1)"
   }
 };
 
@@ -17,9 +25,9 @@ const ActionHands = ({ classes }) => (
   <hend-choice>
     <div>
       <div className={classes.actionBox}>
-        <img alt="Your hand" />
+        <img className={classes.playerHand} alt="Your hand" src={rockImg}/>
         <button className={classes.button}>GO</button>
-        <img alt="Opponents hand" />
+        <img alt="Opponents hand" src={rockImg} />
       </div>
     </div>
   </hend-choice>
