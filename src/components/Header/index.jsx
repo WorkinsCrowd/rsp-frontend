ef8748720088dec07ef14fc6af425946987e93aa
Victor Kozlov <punkrus18@gmail.com>
2018-05-25 08:42:11 +0300

    style: header
    finish stylesheader styles;

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,16 +5,28 @@ import PropTypes from "prop-types";
 import SpinningLogo from "./../SpinningLogo";
 
 const styles = {
-
-
+  title: {
+    "text-transform": "capitalize",
+    "font-size": "3em"
+  },
+  header: {
+    padding: "24px",
+    marginBottom: "32px"
+  },
+  headerBox: {
+    "text-align": "center"
+  },
+  powered: {
+    "text-transform": "uppercase"
+  }
 };
 
 const Header = ({ classes, title }) => (
   <header className={classes.header}>
     <div>
-      <div>
+      <div className={classes.headerBox}>
         <h1 className={classes.title}>{title}</h1>
-        <div>powered by nOS - Blockchain</div>
+        <div className={classes.powered}>powered by nOS - Blockchain</div>
       </div>
       <SpinningLogo />
     </div>
