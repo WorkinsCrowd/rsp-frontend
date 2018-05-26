ae7d307ad7b858a1cc4ca00295674f3a79cce0b5
Victor Kozlov <punkrus18@gmail.com>
2018-05-26 10:45:04 +0300

    add animation to hands;

--- src/components/ActionHands/index.jsx
+++ src/components/ActionHands/index.jsx
@@ -18,20 +18,73 @@ const styles = {
   },
   playerHand: {
     transform: "scaleX(-1)"
+  },
+
+  "hand-left-shake": {
+    animation: "shake-left-a 2s linear both infinite",
+    backfaceVisibility: "hidden",
+    perspective: "1000px"
+  },
+  "hand-right-shake": {
+    animation: "shake-right-a 2s linear both infinite",
+    backfaceVisibility: "hidden",
+    perspective: "1000px"
   }
 };
+class ActionHands extends React.Component {
+  constructor(props) {
+    super(props);
 
-const ActionHands = ({ classes }) => (
-  <hend-choice>
-    <div>
-      <div className={classes.actionBox}>
-        <img className={classes.playerHand} alt="Your hand" src={rockImg}/>
-        <button className={classes.button}>GO</button>
-        <img alt="Opponents hand" src={rockImg} />
-      </div>
-    </div>
-  </hend-choice>
-);
+    this.state = {
+      playerChoiceDone: false,
+      leftHandClass: this.handsClasses(false, "left"),
+      rightHandClass: this.handsClasses(false, "right")
+    };
+  }
+
+  componentDidMount = async () => {};
+
+  handsClasses = (shake, hand) => {
+    const { classes } = this.props;
+    const applied = [];
+
+    if (shake) {
+      applied.push(classes[`hand-${hand}-shake`]);
+    }
+
+    return applied.join(" ");
+  };
+
+  handleGoClick = async e => {
+    await this.setState({
+      playerChoiceDone: true,
+      leftHandClass: this.handsClasses(true, "left"),
+      rightHandClass: this.handsClasses(true, "right")
+    });
+  };
+
+  render = () => {
+    const { classes } = this.props;
+
+    return (
+      <React.Fragment>
+        <div>
+          <div className={classes.actionBox}>
+            <img
+              className={`${this.state.leftHandClass} ${classes.playerHand}`}
+              alt="Your hand"
+              src={rockImg}
+            />
+            <button className={classes.button} onClick={this.handleGoClick}>
+              GO
+            </button>
+            <img className={this.state.rightHandClass} alt="Opponents hand" src={rockImg} />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  };
+}
 
 ActionHands.propTypes = {
   classes: PropTypes.object.isRequired
